90f7bf1dbc8be12d5807dab3e82464f047d25d90
Yahia Fouda <yahiabdelmonem@gmail.com>
2024-09-28 19:07:45 +0300

    2

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -1,10 +1,10 @@
 # shopping_list_manager.py
 
 def display_menu():
-    print("\nShopping List Manager")
-    print("1. Add an item")
-    print("2. Remove an item")
-    print("3. View the list")
+    print("Shopping List Manager")
+    print("1. Add Item")
+    print("2. Remove Item")
+    print("3. View List")
     print("4. Exit")
 
 def add_item(shopping_list):
