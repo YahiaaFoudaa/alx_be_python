82c4f0a04a762b3dcf3774fdd7218944feb33b4e
Yahia Fouda <yahiabdelmonem@gmail.com>
2024-10-02 10:24:53 +0300

    ba4

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -23,6 +23,6 @@ class BankAccount:
             return False
     
     def display_balance(self):
-        print(f"Current balance: {self.account_balance}")
+        print(f"Current Balance: {self.account_balance}")
 
         
