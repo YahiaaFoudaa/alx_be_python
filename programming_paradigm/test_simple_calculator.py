aab2c14aafe96dbf100ab2672f323d886c4c6e39
Yahia Fouda <yahiabdelmonem@gmail.com>
2024-10-02 23:52:46 +0300

    rtc7

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -17,7 +17,7 @@ class TestSimpleCalculator(unittest.TestCase):
     def test_subtraction(self):
         self.assertEqual(self.calc.subtract(10, 5), 5)
         self.assertEqual(self.calc.subtract(-5, 5), 0)
-        self.assertEqual(self.calc.subtract(-5, -5), -10)
+        self.assertEqual(self.calc.subtract(-5, -5), 0)
         self.assertEqual(self.calc.subtract(-5, 0), -5)
 
     def test_multiplication(self):
