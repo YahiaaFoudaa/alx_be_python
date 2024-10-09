661eec4d9d85b774222fb45add1c8519787a7ed0
Yahia Fouda <yahiabdelmonem@gmail.com>
2024-10-09 22:20:05 +0300

    ls2

--- oop/library_system.py
+++ oop/library_system.py
@@ -7,7 +7,7 @@ class EBook(Book):
     def __init__(self, file_size):
         self.file_size = file_size
 
-class PrintBook(book):
+class PrintBook(Book):
     def __init__(self, page_count):
         self.page_count = page_count
 
