ee27ff3736c7d0fe7a3aa38377da1c28fc4d9057
Yahia Fouda <yahiabdelmonem@gmail.com>
2024-10-09 22:29:43 +0300

    ls5

--- oop/library_system.py
+++ oop/library_system.py
@@ -7,7 +7,7 @@ class Book:
         return f"'{self.title}' by {self.author}"
 
 class EBook(Book):
-    def __init__(self, file_size):
+    def __init__(self, title, author, file_size):
         super().__init__(title, author)
         self.file_size = file_size
 
@@ -15,7 +15,7 @@ class EBook(Book):
         return f"{super().__str__()} (EBook, Size: {self.file_size}MB)"
     
 class PrintBook(Book):
-    def __init__(self, page_count):
+    def __init__(self, title, author, page_count):
         super().__init__(title, author)
         self.page_count = page_count
 
